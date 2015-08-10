7c094cca0ed03e9cb7d71b8ef610069062aff433
presly808 <presly808@gmail.com>
2015-08-10 09:51:54 +0300

    first commit

---
+++ src/Task1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by serhii on 09.08.15.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/_1TestVariable.java
@@ -0,0 +1,23 @@
+
+public class _1TestVariable {
+
+    // where is start point?
+    // main method
+    // psvm + Tab
+    public static void main(String[] args) {
+        // variable  name = value
+        // create variable <type> <name> = <value>;
+        // then use via name (read, write)
+        byte b1 = 32;// create variable b1 type byte
+        // byte b1 = 44; will be problem
+
+        // sout + TAB
+        System.out.println(b1); // read variable
+
+        // write into variable
+        b1 = 67;
+
+
+    }
+
+}
